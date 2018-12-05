e0c2bbfd1e235df27a37dd7894c06512e2860ae8
Your Name <wamontgomery0068@gmail.com>
2018-12-05 16:45:38 -0600

    Server Works

--- src/Server/Index.js
+++ src/Server/Index.js
@@ -1,24 +1,41 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const mc = require('./MainController');
-
-const port = 3050;
+const mc = require('./mainController');
 
 const app = express();
+
 app.use( bodyParser.json() );
 app.use( cors() );
 
+// grabSumGrub is going to be my baseURL
+const grabSumGrub = "/api/restaurants";
 
-// GET Request with Associate Endpoint
-app.get('/api/grabsumgrub/restaurant', mc.getRestaurant)
+// Post Request with Associated Endpoint
+// You need to create a mc.something. When finished delete comment.
+// app.post( grabSumGrub, mc);
+
+// Get Request with Associated Endpoint
+// You need to create a mc.something. When finished delete comment.
+app.get( grabSumGrub, mc.getRestaurants);
+
+// ----- Note -----
+// For the put and delete endpoints, we need to add on a url parameter of id.
+// A url paramter can be defined by adding :variableName when making the URL for an endpoint.
+
+// ----- Note -----
+// Look at the difference between put,delete and post,get
+
+// Put Request with Associated Endpoint
+// You need to create a mc.something. When finished delete comment.
+// Reference: node-1-afternoon project
+// app.put( `${grabSumGrub}/:id`, mc);
+
+// Delete Request with Associated Endpoint
+// You need to create a mc.something. When finished delete comment.
+// app.delete( `${grabSumGrub}/:id`, mc);
+
+const port = 3002;
 
 app.listen(port, () => {console.log(`It's Magic!; ${port}`)
-})
-
-//  I am recieiving this error.......
-// 'react-scripts' is not recognized as an internal or external command,
-//  operable program or batch file.
-
-// How I know it doesn't word?
-// I copied my old noDB project server folder into this project and tried to run it and got the same error message.
+});
--- src/Server/MainController.js
+++ src/Server/MainController.js
@@ -1,23 +1,19 @@
 const axios = require('axios');
-const config = { headers: {'user-key': '497757ae741a4e8fddd03e095e3b7bc6'}}
 
 let restaurantData = [];
 
-
-function getRestaurant ( req, res, next ) {
-    axios.get("https://developers.zomato.com/api/v2.1/search?entity_id=1138&entity_type=city&count=20&collection_id=428", config).then( response => {
-        restaurantData = response.data
+getRestaurants = ( req, res, next ) => {
+    axios.get("http://opentable.herokuapp.com/api/restaurants?city=Nashville")
+    .then( results => {
+        restaurantData = results.data
         res.status(200).send(restaurantData)
     }).catch(error => res.status(500).send(error))
-};
+}
+
+// ----- Note -----
+// module.exports is the Node way of exporting code to be used in another file.
+// In this case, it is an object whose values are all functions.
 
 module.exports = {
-    getRestaurant,
+    getRestaurants,
 };
-
-//  I am recieiving this error.......
-// 'react-scripts' is not recognized as an internal or external command,
-//  operable program or batch file.
-
-// How I know it doesn't word?
-// I copied my old noDB project server folder into this project and tried to run it and got the same error message.
