1a407714aac9405019f7186919408c9f36a022f6
Your Name <wamontgomery0068@gmail.com>
2018-12-07 17:48:52 -0600

    Working toward putting data on the screen

--- src/Components/MainContent/Restaurants/RestaurantCard/RestaurantCard.js
+++ src/Components/MainContent/Restaurants/RestaurantCard/RestaurantCard.js
@@ -4,31 +4,70 @@ import React from 'react';
 export default function RestaurantCard (props) {
     return(
         <div className = "Restaurants">
+
         <div className = "Restaurant_Container">
+
             <div className = "Restaurant_Card">
-                <div className = "Page_Count"> Page Count </div>
-                <div className = "Name_Restaurant"> Name </div>
-                <div className = "Image_Restaurant"> Image </div>
-                <div className = "Details_Restaurant"> Details</div>
+
+                <div className = "Page_Count">
+
+                    {/* <p> {props.currentIndex +1 } of {props.totalIndex} </p> */}
+
+                </div>
+
+                <div className = "Name_Restaurant">
+
+                    <p> {props.name} </p>
+
+                </div>
+
+                <div className = "Image_Restaurant"> 
+
+                    <p> {props.image_url} </p>
+
+                </div>
+
+                <div className = "Details_Restaurant"></div>
+
             </div>
+
             <div className = "Button_Card">
+
                 <div className = "Previous_Button">
-                    <p class = "Text_Button"> - </p>
+
+                    <button onClick = { props.handlePreviousClick }> - </button>
+
                 </div>
+
                 <div className = "BeenHere_Button">
+
                     <p class = "Text_Button"> Been Here</p>
+
                 </div>
+
                 <div className = "Next_Button">
-                    <p class = "Text_Button"> + </p>
+
+                    <button onClick = { props.handleNextClick }> + </button>
+
                 </div>
+
             </div>
+
         </div>
+
         <div className = "List_Container">
+
             <div className = "Restaurant_Container">
+
                 <div className = "Restaurant_Card"></div>
+
                 <div className = "Button_Card"></div>
+
             </div>
+
         </div>
+
     </div>
+
     )
-}
+};
