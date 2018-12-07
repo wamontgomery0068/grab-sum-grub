561b33b93ac14b33299a74c0120924769d1532a2
Your Name <wamontgomery0068@gmail.com>
2018-12-07 17:19:35 -0600

    Working on props

---
+++ src/Components/MainContent/Restaurants/RestaurantCard/RestaurantCard.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+
+export default function RestaurantCard (props) {
+    return(
+        <div className = "Restaurant_Container">
+            <div className = "Restaurant_Card">
+                <div classNmae = "Page_Count"> Page Count </div>
+                <div className = "Name_Restaurant"> Name </div>
+                <div className = "Image_Restaurant"> Image </div>
+                <div className = "Details_Restaurant"> Details</div>
+            </div>
+            <div className = "Button_Card">
+                <div className = "Previous_Button">
+                    <p class = "Text_Button"> - </p>
+                </div>
+                <div className = "BeenHere_Button">
+                    <p class = "Text_Button"> Been Here</p>
+                </div>
+                <div className = "Next_Button">
+                    <p class = "Text_Button"> + </p>
+                </div>
+            </div>
+            <div className = "List_Container">
+                <div className = "Restaurant_Container">
+                    <div className = "Restaurant_Card"></div>
+                    <div className ="Button_Card"></div>
+                </div>
+            </div>
+        </div>
+    )
+}
--- src/Components/MainContent/Restaurants/Restaurants.js
+++ src/Components/MainContent/Restaurants/Restaurants.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import RestaurantCard from './RestaurantCard/RestaurantCard';
 
 import './Restaurants.css';
 
@@ -9,6 +10,7 @@ class Restaurants extends Component {
 
         this.state = {
             restaurants: [],
+            counter: 0
         };
     };
 
@@ -18,12 +20,47 @@ class Restaurants extends Component {
         });
     }
 
+    handlePreviousClick () {
+
+        if (this.state.counter === 0) {
+            this.setState ( { counter:0 } )
+        }
+        else {
+            this.setState ( {counter: this.state.counter -1} )
+        };
+    };
+
+    handleNextClick () {
+
+        if (this.state.counter === this.state.restaurants) {
+            this.setState ( { counter:0 } )
+        }
+        else {
+            this.setState ( {counter: this.state.counter +1 } )
+        };
+    };
+
     render(){
         return(
             <div className = "Restaurants">
                 <div className = "Restaurant_Container">
-                    <div className = "Restaurant_Card"></div>
-                    <div className = "Button_Card"></div>
+                    <div className = "Restaurant_Card">
+                        <div className = "Page_Count"> Page Count </div>
+                        <div className = "Name_Restaurant"> Name </div>
+                        <div className = "Image_Restaurant"> Image </div>
+                        <div className = "Details_Restaurant"> Details</div>
+                    </div>
+                    <div className = "Button_Card">
+                        <div className = "Previous_Button">
+                            <p class = "Text_Button"> - </p>
+                        </div>
+                        <div className = "BeenHere_Button">
+                            <p class = "Text_Button"> Been Here</p>
+                        </div>
+                        <div className = "Next_Button">
+                            <p class = "Text_Button"> + </p>
+                        </div>
+                    </div>
                 </div>
                 <div className = "List_Container">
                     <div className = "Restaurant_Container">
